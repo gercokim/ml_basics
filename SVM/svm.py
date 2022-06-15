b98fc996fd71b1f335a12494a988c04e57314c02
Gerald Kim <gckim@andrew.cmu.edu>
2022-06-15 17:09:02 -0400

    Implemented SVM classification model

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,5 +1,7 @@
 import sklearn
-from sklearn import datasets, svm
+from sklearn import datasets, svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
+
 
 # sklearn dataset
 cancer = datasets.load_breast_cancer()
@@ -7,7 +9,6 @@ cancer = datasets.load_breast_cancer()
 # features and targets
 print(cancer.feature_names)
 print(cancer.target_names)
-print("---------------------------")
 
 X = cancer.data
 Y = cancer.target
@@ -18,3 +19,15 @@ print(x_train, y_train)
 
 # for making results more readable, i.e. 0, 1 to malignant, benign
 classes = ['malignant', 'benign']
+
+# creates classifier (support vector classifier) with a linear kernel and an increased soft margin
+clf = svm.SVC(kernel="linear", C=2)
+clf.fit(x_train, y_train)
+
+# calculates predictions from test data
+y_pred = clf.predict(x_test)
+
+# calculates accuracy of model
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
