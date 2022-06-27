f5123ee3d59edeb92d59c69c0313d4fc34d38d4c
Gerald Kim <gckim@andrew.cmu.edu>
2022-06-27 18:52:01 -0400

    Clean up in main spam file

--- rnn/spam.py
+++ rnn/spam.py
@@ -84,32 +84,4 @@ sample_text = 'Congratulations! You won a $100 gift card. Click this link to cla
 predictions = model.predict(np.array([sample_text]))
 print(predictions)
 
-# Predicting ham or spam
-def predict_message(pred_text):
-    return 0
-
-def test_predictions():
-  test_messages = ["how are you doing today",
-                   "sale today! to stop texts call 98912460324",
-                   "i dont want to go. can we try it a different day? available sat",
-                   "our new mobile video service is live. just install on your phone to start watching.",
-                   "you have won £1000 cash! call to claim your prize.",
-                   "i'll bring it tomorrow. don't forget the milk.",
-                   "wow, is your arm alright. that happened to me one time too"
-                  ]
-
-  test_answers = ["ham", "spam", "ham", "spam", "spam", "ham", "ham"]
-  passed = True
-
-  for msg, ans in zip(test_messages, test_answers):
-    prediction = predict_message(msg)
-    if prediction[1] != ans:
-      passed = False
-
-  if passed:
-    print("Tests have passed")
-  else:
-    print("Tests have failed")
-
-#test_predictions()
 
